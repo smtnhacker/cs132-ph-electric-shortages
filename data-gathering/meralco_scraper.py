9e7f3ef2c3f71e59596cd89241be2a2450824cbf
smtnhacker <themikosurara@gmail.com>
2024-03-02 22:41:58 +0800

    feat: scrape affected locations

--- data-gathering/meralco_scraper.py
+++ data-gathering/meralco_scraper.py
@@ -9,12 +9,13 @@ from selenium.webdriver.support import expected_conditions as EC
 driver = webdriver.Chrome()
 
 class DataRow:
-    def __init__(self, date, has_outage):
+    def __init__(self, date, has_outage, affected_locs):
         self.date = date
         self.has_outage = 1 if has_outage else 0
+        self.affected_locs = '|'.join(affected_locs)
     
     def __str__(self):
-        return f"{self.date},{self.has_outage}\n"
+        return f'{self.date},{self.has_outage},"{self.affected_locs}"\n'
 
 def attempt_click(element):
     tries = 0
@@ -36,6 +37,25 @@ def has_maintanance():
     except Exception:
         return True
 
+def get_affected_locs():
+    try:
+        try:
+            load_more = driver.find_element(By.CLASS_NAME, "btn.btn-bordered.load-more")
+            # 2 or more presses changes the page
+            load_more.click()
+            driver.execute_script("window.scrollTo(0, 500);")
+            time.sleep(1)
+        except Exception:
+            # wont try to load next page anymore since it doesnt seem to exist
+            pass
+
+        posts = driver.find_element(By.CLASS_NAME, "views-infinite-scroll-content-wrapper.clearfix")
+        titles = set(x.get_attribute("innerHTML") for x in posts.find_elements(By.CSS_SELECTOR, "a[class='text-default']"))
+        affected_locs = [x.split(' - ', maxsplit=1)[1] for x in titles]
+        return affected_locs
+    except Exception:
+        return []
+
 def scrape_n_days(n=1):
     driver.get("https://company.meralco.com.ph/news-and-advisories/maintenance-schedule")
     driver.execute_script("window.scrollBy(0, 500);")
@@ -51,7 +71,7 @@ def scrape_n_days(n=1):
     cur_day_btn = calendar.find_element(By.CLASS_NAME, "ui-datepicker-days-cell-over.ui-datepicker-today")
     attempt_click(cur_day_btn)
 
-    yield DataRow(date=cur_date, has_outage=has_maintanance())
+    yield DataRow(date=cur_date, has_outage=has_maintanance(), affected_locs=get_affected_locs())
 
     # get the previous days
     for day in range(n-1):
@@ -93,7 +113,9 @@ def scrape_n_days(n=1):
         else:
             attempt_click(prev_day)
 
-        yield DataRow(date=cur_date, has_outage=has_maintanance())
+        time.sleep(1)
+        
+        yield DataRow(date=cur_date, has_outage=has_maintanance(), affected_locs=get_affected_locs())
 
 def main():
     if not os.path.isdir(os.path.join("data-gathering", "data")):
@@ -104,7 +126,7 @@ def main():
     # so be CAREFUL!!!
     open(filepath, 'w').close()
 
-    for dataRow in scrape_n_days(n=100):
+    for dataRow in scrape_n_days(n=5):
         with open(filepath, 'a') as f:
             f.write(str(dataRow))
     
