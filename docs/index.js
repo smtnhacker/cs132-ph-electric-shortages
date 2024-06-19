f311354e4b6c9006535565af372d4d3b2fdcde27
Gabriel Samonte <samontegabriel2001@gmail.com>
2024-06-19 21:55:59 +0800

    feat: update website

--- docs/index.js
+++ docs/index.js
@@ -5,11 +5,28 @@ Array.prototype.forEach.call(links, function (elem, index) {
   if (elemAttr && elemAttr.includes("#")) {
     elem.addEventListener("click", function (ev) {
       ev.preventDefault();
-      document.getElementById(elemAttr.replace(/#/g, "")).scrollIntoView({
+      document.getElementById(elemAttr.replace(/#/, "")).scrollIntoView({
         behavior: "smooth",
         block: "start",
         inline: "nearest",
       });
     });
   }
+});
+
+document.querySelectorAll('a[href^="#"]').forEach((link) => {
+  link.addEventListener("click", function (e) {
+    e.preventDefault(); // Prevent default jump to top
+
+    const target = document.getElementById(
+      this.getAttribute("href").substring(1)
+    );
+    const headerHeight = document.querySelector("header").offsetHeight; // Get header height
+    const targetTop = target.offsetTop - headerHeight; // Calculate offset for header
+
+    window.scrollTo({
+      top: targetTop,
+      behavior: "smooth",
+    });
+  });
 });
